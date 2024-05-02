b58b9fba25ba3f96a38681512765ca429ed67958
Lake <1801700@qq.com>
2024-05-03 00:27:37 +0800

    20240503

--- js/duration.js
+++ js/duration.js
@@ -1,6 +1,6 @@
 !(function() {
     /** 计时起始时间，自行修改 **/
-    var start = new Date("2020/01/01 00:00:00");
+    var start = new Date("2020/03/01 15:04:05");
   
     function update() {
       var now = new Date();
